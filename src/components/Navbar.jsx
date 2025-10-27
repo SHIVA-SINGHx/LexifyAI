3d9d377d2a344968498103e1583bd0df4fde645c
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-27 10:32:38 -0700

    Created sign-in

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -49,8 +49,6 @@ const Navbar = () => {
             
             </Link>
         
-         
-          
             <Button className="bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700">
               Start Free Trial
             </Button>
