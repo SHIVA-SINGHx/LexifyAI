42ce6a2f86b8dea6405e5f940f297e740d8709c2
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-10-31 10:32:09 -0700

    Created history ui

--- src/app/dashboard/history/page.tsx
+++ src/app/dashboard/history/page.tsx
@@ -1,11 +1,40 @@
-import React from 'react'
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs/server";
+// import { format } from "date-fns";
 
-const page = () => {
+const History = async () => {
+
   return (
-    <div>
-      hsitory
-    </div>
-  )
-}
+    <div className="mx-5 py-2">
+      <div className="mt-5 py-6 px-4 bg-white rounded">
+        <h2 className="font-medium">Output History</h2>
+      </div>
+      <div className="mt-5 py-6 px-4 bg-white rounded">
+        <Table>
+          <TableCaption>A list of your ai output history.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Template</TableHead>
+              <TableHead className="w-[250px]">Title</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead className="text-right">Created At</TableHead>
+            </TableRow>
+          </TableHeader>
 
-export default page
+        </Table>
+      </div>
+    </div>
+  );
+};
+
+export default History;
