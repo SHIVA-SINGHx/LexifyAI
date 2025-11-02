269302f723a44fb2b000a010900964560711c90c
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-02 09:44:22 -0800

    created payment structure

---
+++ src/app/dashboard/upgrade/checkout/route.tsx
@@ -0,0 +1,31 @@
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { NextResponse } from "next/server"
+import Stripe from "stripe"
+
+export async function POST(req: Request){
+    try {
+        const {userId} = auth
+        if(!userId){
+            return new NextResponse("Unauthorized",{status: 401} )
+        }
+
+        const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [
+            {
+                quantity: 1,
+                price_data:{
+                    currency: "USD",
+                    product_data: {
+                        name: "10,000 Ai Credit",
+                        description: "all 10$ worth of credit"
+                    },
+                    unit_amount: 1000
+                }
+            }
+        ]
+
+    } catch (error) {
+        return new NextResponse("Internal Server Error: ", {status:500})
+    }
+}
+
